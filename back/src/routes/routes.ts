27d3a1a74fa39fa4336f5a9bcac8917f129b3492
patorodriguez12 <pato_rodriguez12@outlook.com>
2024-12-16 08:42:32 -0300

    back - routes endpoints created (without functionality)

--- back/src/routes/routes.ts
+++ back/src/routes/routes.ts
@@ -2,4 +2,16 @@ import { Router } from "express";
 
 const router = Router();
 
+// users routes
+router.get("/users", () => {})
+router.get("/users/:id", () => {})
+router.post("/users/register", () => {})
+router.post("/users/login", () => {})
+
+// appointments routes
+router.get("/appointments", () => {})
+router.get("/appointments/:id", () => {})
+router.post("/appointments/schedule", () => {})
+router.put("/appointments/cancel", () => {})
+
 export default router;
