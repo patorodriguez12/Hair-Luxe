822222ec3346413d97095a79689dd389d0620b65
patorodriguez12 <pato_rodriguez12@outlook.com>
2024-12-16 08:37:23 -0300

    back - server initial configs (index, server, routes, env)

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,1 +1,6 @@
-console.log("Hola mundo");
+import server from "./server";
+import { PORT } from "./config/envs";
+
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
