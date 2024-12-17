c94ee9a3606f037cdc3042bb9292fa18e074dc80
patorodriguez12 <pato_rodriguez12@outlook.com>
2024-12-17 18:30:30 -0300

    back - start to work with controllers and services

--- back/src/config/data-source.ts
+++ back/src/config/data-source.ts
@@ -4,15 +4,19 @@ import { Appointment } from "../entities/Appointment";
 import { Credential } from "../entities/Credential";
 
 export const AppDataSource = new DataSource({
-    type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "postgres",
-    password: "3996161812",
-    database: "hairluxe",
-    synchronize: true,
-    logging: false,
-    entities: [User, Appointment, Credential],
-    subscribers: [],
-    migrations: [],
-})
+  type: "postgres",
+  host: "localhost",
+  port: 5432,
+  username: "postgres",
+  password: "3996161812",
+  database: "hairluxe",
+  synchronize: true,
+  logging: false,
+  entities: [User, Appointment, Credential],
+  subscribers: [],
+  migrations: [],
+});
+
+export const UserModel = AppDataSource.getRepository(User);
+export const AppointmentModel = AppDataSource.getRepository(Appointment);
+export const CredentialModel = AppDataSource.getRepository(Credential);
