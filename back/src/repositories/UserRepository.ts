0d75ffa4712ad6c45bda830d2d25a9667ef2eb86
patorodriguez12 <pato_rodriguez12@outlook.com>
2024-12-19 19:22:42 -0300

    back - created custom repository methods

--- back/src/repositories/UserRepository.ts
+++ back/src/repositories/UserRepository.ts
@@ -1,6 +1,15 @@
 import { AppDataSource } from "../config/data-source";
 import { User } from "../entities/User";
 
-const UserRepository = AppDataSource.getRepository(User);
+const UserRepository = AppDataSource.getRepository(User).extend({
+  findById: async function (id: number): Promise<User | null> {
+    const user = this.findOneBy({ id });
+    return user;
+  },
+  checkById: async function (id: number): Promise<Boolean> {
+    const user = await this.findById(id);
+    return !!user;
+  },
+});
 
 export default UserRepository;
