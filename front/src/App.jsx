13dc0f4acdb938dbc879f9299ea53552478d34dc
patorodriguez12 <pato_rodriguez12@outlook.com>
2024-12-20 19:16:39 -0300

    front - removed temporaly login register components, created basic components

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,24 +1,14 @@
-import Login from "./components/Login";
-import Register from "./components/Register";
+import Home from "./views/Home";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+import { useState } from "react";
 
 function App() {
-  const isRegistered = true;
-
-  if (isRegistered) {
-    return (
-      <div>
-        <h1>Bienvenidos a mi primer app</h1>
-        <Login />
-      </div>
-    );
-  }
-
   return (
-    <div className="min-h-screen bg-blue-500 flex items-center justify-center">
-      <h1 className="text-white text-4xl font-bold">
-        Bienvenidos a mi primer app
-      </h1>
-      {isRegistered ? <Login /> : <Register />}
+    <div>
+      <NavBar />
+      <Home />
+      <Footer />
     </div>
   );
 }
---
+++ front/src/components/NavBar.jsx
@@ -0,0 +1,9 @@
+function NavBar() {
+  return (
+    <div>
+      <h1>Hair Luxe</h1>
+    </div>
+  );
+}
+
+export default NavBar;
