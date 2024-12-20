e5dd688941caf074c10e3622ae44789cb5ef2705
patorodriguez12 <pato_rodriguez12@outlook.com>
2024-12-20 10:35:53 -0300

    back - userServices basic documentation

--- back/src/services/usersServices.ts
+++ back/src/services/usersServices.ts
@@ -3,36 +3,51 @@ import UserDto from "../dto/UserDto";
 import { User } from "../entities/User";
 const bcrypt = require("bcrypt");
 
+
+// GET ALL USERS SERVICE
 export const getUsersService = async (): Promise<User[]> => {
   const users = await UserRepository.find({ relations: ["appointments"] });
   return users;
 };
 
+// GET USER BY ID SERVICE
+export const getUserByIdService = async (id: number): Promise<User | null> => {
+
+  // find user by id with appointments relation
+  const user = await UserRepository.findOne({
+    where: { id },
+    relations: ["appointments"],
+  });
+
+  // check if the id exists
+  if (!user) {
+    throw new Error(`User with id ${id} not exists`);
+  }
+  return user;
+};
+
+
+// REGISTER USER SERVICE
 export const registerUserService = async (userData: UserDto) => {
+
+  // check if email already exists
   const existingUser = await UserRepository.findOneBy({
     email: userData.email,
   });
+
+  // if email exists, throw error
   if (existingUser) {
     throw new Error(`User with email ${userData.email} already exists.`);
   }
 
+  // hash password
   const hashedPassword = await bcrypt.hash(userData.password, 10);
 
+  // create user with hashed password
   const newUser = UserRepository.create({
     ...userData,
     password: hashedPassword,
   });
   const result = await UserRepository.save(newUser);
   return result;
-};
-
-export const getUserByIdService = async (id: number): Promise<User | null> => {
-  const user = await UserRepository.findOne({
-    where: { id },
-    relations: ["appointments"],
-  });
-  if (!user) {
-    throw new Error(`User with id ${id} not exists`);
-  }
-  return user;
 };
