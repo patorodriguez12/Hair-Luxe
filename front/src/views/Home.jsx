7481d57e2e91fc16870fb2040053cd89e3ff19e7
patorodriguez12 <pato_rodriguez12@outlook.com>
2024-12-21 17:20:38 -0300

    front - making use of myappointments component

--- front/src/views/Home.jsx
+++ front/src/views/Home.jsx
@@ -1,23 +1,7 @@
-import AppointmentCard from "../components/AppointmentCard";
-import { myAppointments } from "../helpers/myAppointments";
-import { useState } from "react";
-
 const Home = () => {
-  const [appointments, setAppointments] = useState(myAppointments);
-
-  const addAppointment = (newAppointment) => {
-    setAppointments([...appointments, newAppointment]);
-  };
-
   return (
     <div id="home" className="bg-gray-100 text-gray-800 py-12">
-      <div className="container mx-auto text-center">
-        <h1 className="text-4xl font-bold mb-4">Welcome to MyApp</h1>
-        {appointments.map((appointment) => (
-          <AppointmentCard key={appointment.id} appointment={appointment} />
-        ))}
-        <button onClick={() => addAppointment({})}>Add appointment</button>
-      </div>
+      <h1>Home</h1>
     </div>
   );
 };
--- front/src/views/MyAppointments.jsx
+++ front/src/views/MyAppointments.jsx
@@ -1,13 +1,23 @@
 import { useState } from "react";
-import myAppointments from "../helpers/myAppointments";
+import {myAppointments} from "../helpers/myAppointments";
 import AppointmentCard from "../components/AppointmentCard";
 
 function MyAppointments() {
   const [appointments, setAppointments] = useState(myAppointments);
 
+  const addAppointment = (newAppointment) => {
+    setAppointments([...appointments, newAppointment]);
+  };
+
   return (
-    <div>
-      <h1>My Appointments</h1>
+    <div id="home" className="bg-gray-100 text-gray-800 py-12">
+      <div className="container mx-auto text-center">
+        <h1 className="text-4xl font-bold mb-4">MIS TURNOS</h1>
+        {appointments.map((appointment) => (
+          <AppointmentCard key={appointment.id} appointment={appointment} />
+        ))}
+        <button onClick={() => addAppointment({})}>Agendar nuevo turno</button>
+      </div>
     </div>
   );
 }
