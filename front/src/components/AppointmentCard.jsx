b47e4edf129e307aa2fe7bf90d2b8802ecc8d1c5
patorodriguez12 <pato_rodriguez12@outlook.com>
2024-12-21 16:33:25 -0300

    front - rendering hardcoded info

--- front/src/components/AppointmentCard.jsx
+++ front/src/components/AppointmentCard.jsx
@@ -1,7 +1,12 @@
-function AppointmentCard() {
+function AppointmentCard({ appointment: { date, time, status } }) {
   return (
-    <div>AppointmentCard</div>
-  )
+    <div>
+      <h2>{date}</h2>
+      <h2>{time}</h2>
+      <h2>{status}</h2>
+    </div>
+  );
 }
 
-export default AppointmentCard
+
+export default AppointmentCard;
--- front/src/views/Home.jsx
+++ front/src/views/Home.jsx
@@ -1,12 +1,22 @@
+import AppointmentCard from "../components/AppointmentCard";
+import { myAppointments } from "../helpers/myAppointments";
+import { useState } from "react";
+
 const Home = () => {
+  const [appointments, setAppointments] = useState(myAppointments);
+
+  const addAppointment = (newAppointment) => {
+    setAppointments([...appointments, newAppointment]);
+  };
+
   return (
     <div id="home" className="bg-gray-100 text-gray-800 py-12">
       <div className="container mx-auto text-center">
         <h1 className="text-4xl font-bold mb-4">Welcome to MyApp</h1>
-        <p className="text-lg mb-6">Manage your appointments with ease.</p>
-        <button className="bg-blue-600 text-white px-6 py-2 rounded hover:bg-blue-500">
-          Get Started
-        </button>
+        {appointments.map((appointment) => (
+          <AppointmentCard key={appointment.id} appointment={appointment} />
+        ))}
+        <button onClick={() => addAppointment({})}>Add appointment</button>
       </div>
     </div>
   );
