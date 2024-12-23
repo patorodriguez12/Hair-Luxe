21a405632c83651af6dee36fe35168b7ae6b7234
patorodriguez12 <pato_rodriguez12@outlook.com>
2024-12-22 21:41:08 -0300

    back - implemented cors middleware config

--- back/src/server.ts
+++ back/src/server.ts
@@ -2,9 +2,15 @@ import express from "express";
 import router from "./routes/routes";
 const cors = require("cors");
 
+const corsOptions = {
+    origin: ["http://localhost:5173"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+}
+
 const server = express();
 server.use(express.json());
 server.use(router);
-server.use(cors());
+server.use(cors(corsOptions));
 
 export default server;
