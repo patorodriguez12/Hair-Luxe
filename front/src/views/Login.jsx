7a3cdc6e256b86a6870a4bc07f19cd9ddb8a469a
patorodriguez12 <pato_rodriguez12@outlook.com>
2024-12-23 10:36:43 -0300

    front - basic functionallity to login form (not sending data to backend yet)

--- front/src/views/Login.jsx
+++ front/src/views/Login.jsx
@@ -1,11 +1,32 @@
+import { useState } from "react";
+
 const Login = () => {
+  const [userData, setUserData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleInputChange = (event) => {
+    const { id, value } = event.target;
+    setUserData({ ...userData, [id]: value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`Email: ${userData.email}, Password: ${userData.password}`);
+    // axios request to login the user
+  };
+
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="bg-white shadow-lg rounded-lg p-8 max-w-md w-full">
         <h1 className="text-2xl font-bold mb-6 text-gray-800 text-center">
           Iniciar sesion
         </h1>
-        <form>
+
+        {/* Login form */}
+        <form onSubmit={handleSubmit}>
+          {/* Email field */}
           <div className="mb-4">
             <label
               htmlFor="email"
@@ -16,10 +37,14 @@ const Login = () => {
             <input
               type="email"
               id="email"
-              placeholder="Enter your email"
+              placeholder="Ingresa tu correo electronico"
               className="mt-1 block w-full p-2 border border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500"
+              value={userData.email}
+              onChange={handleInputChange}
             />
           </div>
+
+          {/* Password field */}
           <div className="mb-6">
             <label
               htmlFor="password"
@@ -30,10 +55,14 @@ const Login = () => {
             <input
               type="password"
               id="password"
-              placeholder="Enter your password"
+              placeholder="Ingresa tu contraseña"
               className="mt-1 block w-full p-2 border border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500"
+              value={userData.password}
+              onChange={handleInputChange}
             />
           </div>
+
+          {/* Submit button */}
           <button
             type="submit"
             className="w-full bg-blue-600 text-white py-2 px-4 rounded-md hover:bg-blue-700 transition duration-200"
@@ -41,6 +70,8 @@ const Login = () => {
             Iniciar sesion
           </button>
         </form>
+
+        {/* Register link */}
         <p className="text-sm text-gray-600 mt-4 text-center">
           No tienes una cuenta?{" "}
           <a href="/register" className="text-blue-500 hover:underline">
