ef9e0147501eb8cb038eeaab1ffd7d940f0cf76a
patorodriguez12 <pato_rodriguez12@outlook.com>
2024-12-27 10:58:08 -0300

    front - now we fetch user data on MyAppointments to update user appointments

--- front/src/views/MyAppointments.jsx
+++ front/src/views/MyAppointments.jsx
@@ -1,13 +1,47 @@
-import { useContext, useState } from "react";
+import { useContext, useState, useEffect } from "react";
 import AppointmentCard from "../components/AppointmentCard";
 import AppointmentDetail from "../components/AppointmentDetail";
 import { AuthContext } from "../context/AuthContext";
 import { Link } from "react-router-dom";
+import Cookies from "js-cookie";
+import axios from "axios";
 
 const MyAppointments = () => {
   const [detail, setDetail] = useState(false);
   const [id, setId] = useState(null);
-  const { currentUser } = useContext(AuthContext);
+  const { currentUser, setCurrentUser } = useContext(AuthContext);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchUserAppointments = async () => {
+      try {
+        const token = Cookies.get("token");
+        const response = await axios.get(`/users/${currentUser.id}`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        setCurrentUser(response.data);
+        setLoading(false);
+      } catch (err) {
+        console.error("Error fetching user appointments:", err);
+        setError("Error al cargar los turnos.");
+        setLoading(false);
+      }
+    };
+
+    fetchUserAppointments();
+  }, [setCurrentUser, currentUser.id]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>{error}</div>;
+  }
+
   const appointments = currentUser.appointments;
 
   const handleOnClick = (appointmentId) => {
